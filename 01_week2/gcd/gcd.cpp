14aabd10d917bba9153612e2d5fa30fd6a0e7aeb
SnehaMidha <snehamidha32@gmail.com>
2017-02-17 20:13:06 +0530

    greatest common divisor

---
+++ 01_week2/gcd/gcd.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int gcd_naive(int a, int b) {
+	int current_gcd = 1;
+	for (int d = 2; d <= a && d <= b; d++) {
+		if (a % d == 0 && b % d == 0) {
+			if (d > current_gcd) {
+				current_gcd = d;
+			}
+		}
+	}
+	return current_gcd;
+}
+
+int gcd(int a, int b){
+	int current_gcd=a;
+	int rem,div;
+	div=b;
+	if(a==0)
+		return b;
+	if(b==0)
+		return a;
+//	if(a<b)
+//	{ current_gcd=a;
+//		div=b;}
+//	else
+//	{ current_gcd=b;
+//		div=a;}
+	rem=div%current_gcd;
+	while(rem!=0){
+		div= current_gcd;
+		current_gcd=rem;
+		rem=div%current_gcd;
+	}
+	return current_gcd;
+}
+
+int main() {
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << gcd_naive(a, b) << std::endl;
+ std::cout << gcd(a, b) << std::endl;
+	return 0;
+}
