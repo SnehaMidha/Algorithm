348a773631326edd68562240bb1676ff231617c8
SnehaMidha <snehamidha32@gmail.com>
2017-02-19 12:28:36 +0530

    maximizing your salary

---
+++ 02_greedy_algorithms/largest_number/largest_number.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+bool isgreaterorequal(string d1,string d2)
+{
+	string a,b;
+	if(d1.length()==d2.length())
+	{
+		a=d1;
+		b=d2;
+	}
+	else 
+	{
+		a=d1+d2;
+		b=d2+d1;
+	}
+	int c=a.compare(b);
+	return c1>=0;
+}
+string largest_number(vector<string> a) {
+	//write your code here
+	sort(a.begin(),a.end(),isgreaterorequal);
+
+
+	std::stringstream ret;
+	for (size_t i = 0; i < a.size(); i++) {
+		ret << a[i];
+	}
+	string result;
+	ret >> result;
+	return result;
+}
+
+int main() {
+	int n;
+	std::cin >> n;
+	vector<string> a(n);
+	for (size_t i = 0; i < a.size(); i++) {
+		std::cin >> a[i];
+	}
+	std::cout << largest_number(a);
+}
