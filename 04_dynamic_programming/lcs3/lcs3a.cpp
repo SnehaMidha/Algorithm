0a628e16bca80e56544be6f68dfd87a4526fe53a
Sneha <snehamidha32@gmail.com>
2017-02-21 18:13:45 +0530

    added python code

--- 04_dynamic_programming/lcs3/lcs3a.cpp
+++ 04_dynamic_programming/lcs3/lcs3a.cpp
@@ -1,35 +1,13 @@
+//Task. Given three sequences A = (a 1 , a 2 , . . . , a n ), B = (b 1 , b 2 , . . . , b m ), and C = (c 1 , c 2 , . . . , c l ), find the length of their longest common subsequence, i.e., the largest non-negative integer p such that there exist indices 1 ≤ i 1 < i 2 < · · · < i p ≤ n, 1 ≤ j 1 < j 2 < · · · < j p ≤ m, 1 ≤ k 1 < k 2 < · · · < k p ≤ l such that a i 1 = b j 1 = c k 1 , . . . , a i p = b j p = c k p
+
 #include <iostream>
 #include <vector>
 
 using std::vector;
 
-int edit_distance(vector<int> &str1, vector<int> &str2) {
-        //write your code here
-        int m=str1.size(),n=str2.size();
-        vector <vector <int> > D(m+1,vector<int>(n+1,0));
-        /*for(int i=0;i<=m;i++)
-                D[i][0]=i;
-        for(int j=0;j<=n;j++)
-                D[0][j]=j;*/
-        int in,del,ma,mm;
-        for(int i=1;i<=m;i++)
-                for(int j=1;j<=n;j++)
-                {
-                        in=D[i][j-1];
-                        del=D[i-1][j];
-                        ma=D[i-1][j-1]+1;
-                        mm=D[i-1][j-1];
-                        if(str1[i]==str2[j])
-                                D[i][j]=std::max(in,std::max(del,ma));
-                        else
-                                D[i][j]=std::max(in,std::max(del,mm));
-                }
 
-
-        return D[m][n];
-}
 int lcs3(vector<int> &a, vector<int> &b, vector<int> &c) {
-  vector<vector<vector<int> > > D(a.size()+1,vector<vector<int> >(b.size()+1,vector<int>(c.size()+1,0)));
+	vector<vector<vector<int> > > D(a.size()+1,vector<vector<int> >(b.size()+1,vector<int>(c.size()+1,0)));
 	int x,y,z,m;
 	for(int i=1;i<=a.size();i++)
 		for(int j=1;j<=b.size();j++)
@@ -41,9 +19,9 @@ int lcs3(vector<int> &a, vector<int> &b, vector<int> &c) {
 				m=D[i-1][j-1][k-1];
 				if(a[i-1]==b[j-1] && b[j-1]==c[k-1])
 				{D[i][j][k]=m+1;
-			       std::cout<<a[i-1]<<std::endl;}
+					std::cout<<a[i-1]<<std::endl;}
 				else
-				D[i][j][k]=std::max(x,std::max(y,z));
+					D[i][j][k]=std::max(x,std::max(y,z));
 
 			}
 
@@ -51,26 +29,26 @@ int lcs3(vector<int> &a, vector<int> &b, vector<int> &c) {
 }
 
 int main() {
-  size_t an;
-  std::cin >> an;
-  vector<int> a(an);
-  for (size_t i = 0; i < an; i++) {
-    std::cin >> a[i];
-  }
-  size_t bn;
-  std::cin >> bn;
-  vector<int> b(bn);
-  for (size_t i = 0; i < bn; i++) {
-    std::cin >> b[i];
-  }
-  size_t cn;
-  std::cin >> cn;
-  vector<int> c(cn);
-  for (size_t i = 0; i < cn; i++) {
-    std::cin >> c[i];
-  }
-  std::cout << lcs3(a, b, c) << std::endl;
-//	std::cout<<edit_distance(a,b);
+	size_t an;
+	std::cin >> an;
+	vector<int> a(an);
+	for (size_t i = 0; i < an; i++) {
+		std::cin >> a[i];
+	}
+	size_t bn;
+	std::cin >> bn;
+	vector<int> b(bn);
+	for (size_t i = 0; i < bn; i++) {
+		std::cin >> b[i];
+	}
+	size_t cn;
+	std::cin >> cn;
+	vector<int> c(cn);
+	for (size_t i = 0; i < cn; i++) {
+		std::cin >> c[i];
+	}
+	std::cout << lcs3(a, b, c) << std::endl;
+	//	std::cout<<edit_distance(a,b);
 
 
 }
